6a5f08495d4c794ba06ae8a547c808d20a248b12
Justin Hutsell <hutselljustin@gmail.com>
2019-06-06 20:45:37 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song 
+  
+  
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+  
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@count += 1 
+  end
+  
+  def self.count 
+    @@count 
+  end
+  
+  def self.artists 
+    @@artists 
+  end
+  
+  def self.genres 
+    @@genres 
+  end
+  
+  def self.genre_count 
+    
+  end
+  
+  def self.artist_count 
+    
+  end
+  
+  
+  
+end
